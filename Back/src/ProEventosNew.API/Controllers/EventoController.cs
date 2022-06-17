77725ddf5ef64bc80e9751ecb5552528911d186b
Bexus Soluções Digitais <leandro@bexus.com.br>
2022-06-17 17:48:34 -0300

    Seção 2: AS.Net 5

---
+++ Back/src/ProEventosNew.API/Controllers/EventoController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventosNew.API.Models;
+
+namespace ProEventosNew.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento []{
+            new Evento(){
+                EventoId = 1,
+                Tema = "Angular 11 e .NET 5",
+                Local = "Belo Horizonte",
+                Lote = "1º Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                ImagemURL = "foto.png"
+            },
+            new Evento(){
+                EventoId = 2,
+                Tema = "Angular e suas novidades.",
+                Local = "São PAulo",
+                Lote = "2º Lote",
+                QtdPessoas = 350,
+                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+                ImagemURL = "foto1.png"
+            }
+           };
+        public EventoController(ILogger<EventoController> logger)
+        {
+          
+        }
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+           return _evento;
+        }
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+        return _evento.Where(evento => evento.EventoId == id);
+        }
+        [HttpPost]
+        public string Post()
+        {
+           return "Exemplo de Post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int  id)
+        {
+           return $"Exemplo de Put com id = {id}.";
+        }
+
+        [HttpDelete("{id}")]
+         public string Delete(int id)
+        {
+            return $"Exemplo de Delete  com id = {id}.";
+        }
+    }
+   
+}
---
+++ Back/src/ProEventosNew.API/Models/Evento.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventosNew.API.Models
+{
+    public class Evento
+    {
+        public int EventoId { get; set;}
+
+        public string Local { get; set;}
+
+        public string DataEvento { get; set;}
+
+        public string Tema { get; set;}
+
+        public int QtdPessoas { get; set;}
+
+        public string Lote { get; set;}
+
+        public string ImagemURL { get; set;}
+
+    }
+}
